9f97586aa6d195d5047e9cbb65d2882f6170fa00
crbazevedo <renatoaz@gmail.com>
2016-03-31 21:09:02 -0300

    Adding SMS-EMOA example run

--- moo.py
+++ moo.py
@@ -1,4 +1,21 @@
 #!/usr/bin/phyton
-# Hello world Python program
+# SMS-EMOA example
 
-print "Hello World"
+import random
+from optproblems import Problem
+from evoalgos.algo import SMSEMOA
+from evoalgos.individual import SBXIndividual
+
+def obj_function(phenome):
+    return sum(x ** 2 for x in phenome), sum((x - 2) ** 2 for x in phenome)
+
+problem = Problem(obj_function, num_objectives=2, max_evaluations=1000, name="Example")
+popsize = 10
+population = []
+for _ in range(popsize):
+    population.append(SBXIndividual(genome=[random.random() * 5 for _ in range(10)]))
+
+ea = SMSEMOA(problem, population, popsize)
+ea.run()
+for individual in population:
+    print(individual)
